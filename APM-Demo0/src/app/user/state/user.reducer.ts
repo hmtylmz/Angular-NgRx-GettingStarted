d0de83190907e647a24d4a61aaa01f23ec9bb2c1
Hamit YILMAZ <hamityilmaz@outlook.com>
2019-12-25 18:03:42 +0300

    Add login page mask username store implementation

---
+++ APM-Demo0/src/app/user/state/user.reducer.ts
@@ -0,0 +1,8 @@
+export function userReducer(state, action) {
+  switch (action.type) {
+    case 'MASK_USERNAME':
+      return { ...state, maskUserName: action.payload };
+    default:
+      return state;
+  }
+}
