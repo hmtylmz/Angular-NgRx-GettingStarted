7e56035e5add252eda182c5d7bd4247545c3d114
Hamit YILMAZ <hamityilmaz@outlook.com>
2019-12-26 12:13:37 +0300

    Fix interface lazy load problems

--- APM-Demo0/src/app/state/app.state.ts
+++ APM-Demo0/src/app/state/app.state.ts
@@ -1,8 +1,6 @@
-import { ProductState } from '../products/state/product.reducer';
 import { UserState } from '../user/state/user.reducer';
 
 export interface State {
-  products: ProductState;
   user: UserState;
 }
 
