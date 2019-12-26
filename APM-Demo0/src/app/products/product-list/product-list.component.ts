82c0016b94b3d41a48bb22f26de1fa738417fe9a
Hamit YILMAZ <hamityilmaz@outlook.com>
2019-12-26 14:35:12 +0300

    Add product state initial state

--- APM-Demo0/src/app/products/product-list/product-list.component.ts
+++ APM-Demo0/src/app/products/product-list/product-list.component.ts
@@ -39,9 +39,7 @@ export class ProductListComponent implements OnInit, OnDestroy {
     });
 
     this.store.pipe(select('products')).subscribe((products) => {
-      if (products) {
-        this.displayCode = products.showProductCode;
-      }
+      this.displayCode = products.showProductCode;
     });
   }
 
