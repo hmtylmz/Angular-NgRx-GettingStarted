6218137c8e9b712b235501c89b3fc9a97a8d39ec
Hamit YILMAZ <hamityilmaz@outlook.com>
2019-12-26 12:19:36 +0300

    Update product state usages with strongly type

--- APM-Demo0/src/app/products/product-list/product-list.component.ts
+++ APM-Demo0/src/app/products/product-list/product-list.component.ts
@@ -5,6 +5,7 @@ import { Subscription } from 'rxjs';
 import { Product } from '../product';
 import { ProductService } from '../product.service';
 import { Store, select } from '@ngrx/store';
+import * as fromProducts from '../state/product.reducer';
 
 @Component({
   selector: 'pm-product-list',
@@ -24,7 +25,7 @@ export class ProductListComponent implements OnInit, OnDestroy {
   sub: Subscription;
 
   constructor(
-    private store: Store<any>,
+    private store: Store<fromProducts.State>,
     private productService: ProductService) { }
 
   ngOnInit(): void {
