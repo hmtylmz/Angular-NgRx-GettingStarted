89351309079bd6db773be9bccb8693df45e94e5d
Hamit YILMAZ <hamityilmaz@outlook.com>
2020-01-02 14:25:44 +0300

    Add update product effects

--- APM-Demo0/src/app/products/state/product.effects.ts
+++ APM-Demo0/src/app/products/state/product.effects.ts
@@ -2,9 +2,10 @@ import { Injectable } from '@angular/core';
 import { ProductService } from '../product.service';
 
 import { Actions, Effect, ofType } from '@ngrx/effects';
-import { ProductActionTypes, LoadSuccess, LoadFail } from './product.actions';
+import { ProductActionTypes, LoadSuccess, LoadFail, UpdateSuccess, UpdateFail } from './product.actions';
 import { mergeMap, map, catchError } from 'rxjs/operators';
 import { of } from 'rxjs';
+import { Product } from '../product';
 
 @Injectable()
 export class ProductEffects {
@@ -20,6 +21,17 @@ export class ProductEffects {
       this.productService.getProducts().pipe(
         map((products) => new LoadSuccess(products)),
         catchError((error) => of(new LoadFail(error)))
+      )
+    )
+  );
+
+  @Effect()
+  updateProduct$ = this.actions$.pipe(
+    ofType(ProductActionTypes.UpdateProduct),
+    mergeMap((product: Product) =>
+      this.productService.updateProduct(product).pipe(
+        map((updatedProduct) => new UpdateSuccess(updatedProduct)),
+        catchError((error) => of(new UpdateFail(error)))
       )
     )
   );
